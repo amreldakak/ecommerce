075b7f78207c1be962273bca3dc63e073d7fb10d
Amr Dakak <107150836+amreldakak@users.noreply.github.com>
2024-03-30 21:14:46 +0200

    3rd commit

---
+++ src/module/auth/auth.Validation.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const signUpVal = Joi.object({
+    username:Joi.string().min(3).max(12).required(),
+    email: Joi.string().email().required(),
+    password:Joi.string().required(),
+    number:Joi.string().required(),
+    role:Joi.string().valid("User"),
+
+});
+
+export const signInVal = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+})
+
+
--- src/module/auth/auth.routes.js
+++ src/module/auth/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { signIn, signUp } from "./auth.controller.js";
+import { validation } from "../middleware/validation.js"
+import { signInVal, signUpVal } from "./auth.Validation.js";
 
 
 const authRoute = express.Router();
 
 
-authRoute.post("/signup",signUp)
-authRoute.post("/signin",signIn)
+authRoute.post("/signup",validation(signUpVal),signUp)
+authRoute.post("/signin",validation(signInVal),signIn)
 
 export default authRoute;
